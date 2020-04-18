c348e2b82425c85d7f8105d84d16ad7b16c4032d
seidel-michael <github@seidel-michael.de>
2020-04-18 21:18:12 +0200

    chore: Add dev docker-compose file for mongodb

---
+++ docker-db/init-mongo.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "devuser",
+        pwd: "devuser",
+        roles: [
+            {
+                role: "readWrite",
+                db: "yadu"
+            }
+        ]
+    }
+)
