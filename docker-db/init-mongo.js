9420a82fcdb29480ae5ba8907f5520b725d693ab
seidel-michael <github@seidel-michael.de>
2020-06-11 15:36:06 +0200

    refactor: Fix eslint warnings

--- docker-db/init-mongo.js
+++ docker-db/init-mongo.js
@@ -1,12 +1,10 @@
-db.createUser(
+db.createUser({
+  user: 'devuser',
+  pwd: 'devuser',
+  roles: [
     {
-        user: "devuser",
-        pwd: "devuser",
-        roles: [
-            {
-                role: "readWrite",
-                db: "yadu"
-            }
-        ]
-    }
-)
+      role: 'readWrite',
+      db: 'yadu',
+    },
+  ],
+});
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import mongoose from 'mongoose';
-import { UserController } from "./controller/user-controller";
+import {UserController} from './controller/user-controller';
 import User from './db/models/user';
 
-mongoose.connect('mongodb://devuser:devuser@localhost:27017/yadu').then(async () => {
+mongoose
+  .connect('mongodb://devuser:devuser@localhost:27017/yadu')
+  .then(async () => {
     console.log('Connected');
 
     const userControler = new UserController();
     await User.create({
-        username: "Test",
-        password: "hash"
+      username: 'Test',
+      password: 'hash',
     });
     const users = await userControler.getUsers();
     console.log(users);
-}).catch(error => console.error(error));
+  })
+  .catch(error => console.error(error));
