d63cc1afce0a9ef05eedfab00d99e817f393223f
seidel-michael <github@seidel-michael.de>
2020-06-11 16:31:05 +0200

    feat: Implement getUserByName

--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -4,12 +4,26 @@ export class UserController {
   constructor() {}
 
   async getUsers(): Promise<UserSchema[]> {
-    const user = await User.find();
-    return user;
+    try {
+      const user = await User.find();
+      return user;
+    } catch (error) {
+      throw new Error(`DBError: ${error.message}`);
+    }
   }
 
   async getUserByName(username: string): Promise<UserSchema> {
-    return Promise.reject(new Error('Not implemented'));
+    let user;
+    try {
+      user = await User.findOne({username});
+    } catch (error) {
+      throw new Error(`DBError: ${error.message}`);
+    }
+    if (!user) {
+      throw new Error('UserNotFound');
+    }
+
+    return user;
   }
 
   async getUserById(id: string): Promise<UserSchema> {
