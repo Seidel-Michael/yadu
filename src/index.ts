ed2496a83d158591e6747d2aa1554ea78efd2ed1
seidel-michael <github@seidel-michael.de>
2020-06-11 20:08:06 +0200

    feat: Add inital admin account creation

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,12 @@
 import mongoose from 'mongoose';
-/*import {UserController} from './controller/user-controller';
-import User from './db/models/user';*/
 import {App} from './app';
+import {initialSetup} from './initial-setup';
 
 mongoose
   .connect('mongodb://devuser:devuser@localhost:27017/yadu')
   .then(async () => {
-    console.log('Connected');
-
+    console.log('Connected to the Database');
+    await initialSetup();
     new App();
-
-    /*const userControler = new UserController();
-    await User.create({
-      username: 'admin',
-      password:
-        '$argon2i$v=19$m=16,t=2,p=1$bFV2cGdVSmpFc1AySzJmcQ$ghjuiA+Uyqlrb8MzJDBAew',
-      groups: [],
-    });
-    const users = await userControler.getUsers();
-    console.log(users);*/
   })
   .catch(error => console.error(error));
